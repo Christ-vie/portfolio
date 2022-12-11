d2c6c7ac9c43c211ed59b32e1df2d0a1c83ee9c9
christmiller <christmiller@gmail.com>
2022-12-11 20:27:13 +0100

    last commit

---
+++ src/routes/About.tsx
@@ -0,0 +1,290 @@
+function About() {
+  return (
+    <>
+      <h1 className="about_h">COMPETENCES & AUTRES...</h1>
+      <div className="comp_l">
+         <div >
+            <img
+              src="https://zupimages.net/up/22/49/9o4w.png"
+              alt="user avatar"
+              className="logo"
+             
+            />
+         </div>
+         <div >
+            <img
+              src="https://zupimages.net/up/22/49/wq2x.jpg"
+              alt="user avatar"
+              className="logo"
+           
+            />
+      </div>
+         <div >
+            <img
+              src="https://zupimages.net/up/22/49/wq2x.jpg"
+              alt="user avatar"
+              className="logo"
+              
+            />
+          </div>
+             <div >
+            <img
+              src="https://zupimages.net/up/22/49/wq2x.jpg"
+              alt="user avatar"
+              className="logo"
+           
+            />
+      </div>
+         <div>
+            <img
+              src="https://zupimages.net/up/22/49/wq2x.jpg"
+              alt="user avatar"
+              className="logo"
+             
+            />
+          </div>
+      </div>
+       
+      <br></br>
+      {/* TO DO PROJECTS */}
+      <h4 className="about_h1">PROJETS</h4>
+      <div className="album py-5 bg-light">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4">
+              <div className="card mb-4 shadow-sm">
+                <svg
+                  className="bd-placeholder-img card-img-top"
+                  width="100%"
+                  height="225"
+                  xmlns="http://www.w3.org/2000/svg"
+                  preserveAspectRatio="xMidYMid slice"
+                  focusable="false"
+                  role="img"
+                  aria-label="Placeholder: Thumbnail"
+                >
+                  <title>Placeholder</title>
+                  <rect width="100%" height="100%" fill="#55595c"></rect>
+                  <text x="50%" y="50%" fill="#eceeef" dy=".3em">
+                    Thumbnail
+                  </text>
+                </svg>
+                <div className="card-body">
+                  <p className="card-text">
+                    This is a wider card with supporting text below as a natural
+                    lead-in to additional content. This content is a little bit
+                    longer.
+                  </p>
+                  <div className="d-flex justify-content-between align-items-center">
+                    <div className="btn-group">
+                      <button
+                        type="button"
+                        className="btn btn-sm btn-outline-secondary"
+                      >
+                        View
+                      </button>
+                    </div>
+                    <small className="text-muted">2022</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="card mb-4 shadow-sm">
+                <svg
+                  className="bd-placeholder-img card-img-top"
+                  width="100%"
+                  height="225"
+                  xmlns="http://www.w3.org/2000/svg"
+                  preserveAspectRatio="xMidYMid slice"
+                  focusable="false"
+                  role="img"
+                  aria-label="Placeholder: Thumbnail"
+                >
+                  <title>Placeholder</title>
+                  <rect width="100%" height="100%" fill="#55595c"></rect>
+                  <text x="50%" y="50%" fill="#eceeef" dy=".3em">
+                    Thumbnail
+                  </text>
+                </svg>
+                <div className="card-body">
+                  <p className="card-text">
+                    This is a wider card with supporting text below as a natural
+                    lead-in to additional content. This content is a little bit
+                    longer.
+                  </p>
+                  <div className="d-flex justify-content-between align-items-center">
+                    <div className="btn-group">
+                      <button
+                        type="button"
+                        className="btn btn-sm btn-outline-secondary"
+                      >
+                        View
+                      </button>
+                    </div>
+                    <small className="text-muted">2022</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="card mb-4 shadow-sm">
+                <svg
+                  className="bd-placeholder-img card-img-top"
+                  width="100%"
+                  height="225"
+                  xmlns="http://www.w3.org/2000/svg"
+                  preserveAspectRatio="xMidYMid slice"
+                  focusable="false"
+                  role="img"
+                  aria-label="Placeholder: Thumbnail"
+                >
+                  <title>Placeholder</title>
+                  <rect width="100%" height="100%" fill="#55595c"></rect>
+                  <text x="50%" y="50%" fill="#eceeef" dy=".3em">
+                    Thumbnail
+                  </text>
+                </svg>
+                <div className="card-body">
+                  <p className="card-text">
+                    This is a wider card with supporting text below as a natural
+                    lead-in to additional content. This content is a little bit
+                    longer.
+                  </p>
+                  <div className="d-flex justify-content-between align-items-center">
+                    <div className="btn-group">
+                      <button
+                        type="button"
+                        className="btn btn-sm btn-outline-secondary"
+                      >
+                        View
+                      </button>
+                    </div>
+                    <small className="text-muted">2022</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-md-4">
+              <div className="card mb-4 shadow-sm">
+                <svg
+                  className="bd-placeholder-img card-img-top"
+                  width="100%"
+                  height="225"
+                  xmlns="http://www.w3.org/2000/svg"
+                  preserveAspectRatio="xMidYMid slice"
+                  focusable="false"
+                  role="img"
+                  aria-label="Placeholder: Thumbnail"
+                >
+                  <title>Placeholder</title>
+                  <rect width="100%" height="100%" fill="#55595c"></rect>
+                  <text x="50%" y="50%" fill="#eceeef" dy=".3em">
+                    Thumbnail
+                  </text>
+                </svg>
+                <div className="card-body">
+                  <p className="card-text">
+                    This is a wider card with supporting text below as a natural
+                    lead-in to additional content. This content is a little bit
+                    longer.
+                  </p>
+                  <div className="d-flex justify-content-between align-items-center">
+                    <div className="btn-group">
+                      <button
+                        type="button"
+                        className="btn btn-sm btn-outline-secondary"
+                      >
+                        View
+                      </button>
+                    </div>
+                    <small className="text-muted">2022</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="card mb-4 shadow-sm">
+                <svg
+                  className="bd-placeholder-img card-img-top"
+                  width="100%"
+                  height="225"
+                  xmlns="http://www.w3.org/2000/svg"
+                  preserveAspectRatio="xMidYMid slice"
+                  focusable="false"
+                  role="img"
+                  aria-label="Placeholder: Thumbnail"
+                >
+                  <title>Placeholder</title>
+                  <rect width="100%" height="100%" fill="#55595c"></rect>
+                  <text x="50%" y="50%" fill="#eceeef" dy=".3em">
+                    Thumbnail
+                  </text>
+                </svg>
+                <div className="card-body">
+                  <p className="card-text">
+                    This is a wider card with supporting text below as a natural
+                    lead-in to additional content. This content is a little bit
+                    longer.
+                  </p>
+                  <div className="d-flex justify-content-between align-items-center">
+                    <div className="btn-group">
+                      <button
+                        type="button"
+                        className="btn btn-sm btn-outline-secondary"
+                      >
+                        View
+                      </button>
+                    </div>
+                    <small className="text-muted">2022</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="card mb-4 shadow-sm">
+                <svg
+                  className="bd-placeholder-img card-img-top"
+                  width="100%"
+                  height="225"
+                  xmlns="http://www.w3.org/2000/svg"
+                  preserveAspectRatio="xMidYMid slice"
+                  focusable="false"
+                  role="img"
+                  aria-label="Placeholder: Thumbnail"
+                >
+                  <title>Placeholder</title>
+                  <rect width="100%" height="100%" fill="#55595c"></rect>
+                  <text x="50%" y="50%" fill="#eceeef" dy=".3em">
+                    Thumbnail
+                  </text>
+                </svg>
+                <div className="card-body">
+                  <p className="card-text">
+                    This is a wider card with supporting text below as a natural
+                    lead-in to additional content. This content is a little bit
+                    longer.
+                  </p>
+                  <div className="d-flex justify-content-between align-items-center">
+                    <div className="btn-group">
+                      <button
+                        type="button"
+                        className="btn btn-sm btn-outline-secondary"
+                      >
+                        View
+                      </button>
+                    </div>
+                    <small className="text-muted">2022</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default About;
---
+++ src/routes/Home.tsx
@@ -0,0 +1,205 @@
+import { FcCallback, FcHome } from "react-icons/fc";
+import { GoMail,GoBriefcase,GoBook } from "react-icons/go";
+
+function Home() {
+  return (
+    <>
+      <div>
+        <div className="card_left">
+          <div className="profil">
+            <img
+              src="https://zupimages.net/up/22/49/wq2x.jpg"
+              alt="user avatar"
+              className="profil_card"
+              width={207}
+              height={200}
+            />
+          </div>
+          <div className="title_card_left">
+            <h2 className="nom">LUPITISA MAYIKA Christ-vie</h2>
+            <ul className="list">
+              <li>
+                <GoMail />: clupitisa@gmail.com
+              </li>
+
+              <li>
+                <FcCallback />: 0651076665
+              </li>
+              <li>
+                <FcHome />: 2 boulevard Georges Méliès, 94360 BRY-SUR-MARNE
+              </li>
+            </ul>
+          </div>
+          <hr className="trace"></hr>
+
+          <h4 className="competence">Expériences <GoBriefcase/></h4>
+          <hr className="competence_hr"></hr>
+          <div className="flex">
+              <div className="card_competences">
+             <div className="paragraph">
+              <h3 className="titre_un">Assitant chef Projet</h3>
+              <p>LUDENDO (LA GRANDE RECRE) </p>
+              <p>Objet :Création d’outil Export et Import de donnée & lamise à jour de BDD </p>
+              <ul><li>
+                Création d’outil de suivi </li>
+                <li>Gestion de bases de données (ACCES/MySQL)</li>
+                <li>
+                  Rédaction de procédure /Gestion de projet (Agile, Scrum)</li>
+                <li>
+                  Automatisation de la mise à jour de Dashboard(Python,VBA) </li><li>
+                  Support des outils informatiques et traitement de mails </li><li>
+                  Pilotage de projet    
+                </li>
+              </ul>
+              <hr className="date_annee"></hr>
+            <h4>SERRIS FRANCE  - Nov 2021 à Sept 2023</h4>
+           
+             </div>
+          </div>
+          <div className="card_competences">
+             <div className="paragraph">
+              <h3 className="titre_un">Assistant chef de Projet</h3>
+              <p>On Tower France (Cellnex)</p>
+              <p>Objet : Création d’outil Exportet Import de donnée & la mise à jour de BDD </p>
+              <ul><li> Création d’outil de suivi  </li>
+                <li>Gestion de bases de données (ACCES/MySQL) </li>
+                <li>
+                  Rédaction de procédure /Gestion de projet (Agile, Scrum)</li>
+                <li>
+                  Automatisation de la mise à jour de Dashboard(Python,VBA) </li><li>
+                  Support des outils informatiques et traitement de mails </li><li>
+           Importetexportdedonnées(Python,VBA,ReactJS)
+                </li>
+              </ul>
+              <hr className="date_annee"></hr>
+            <h4> B. Billancourt - Mai 2021 à octobre 2021</h4>
+           
+             </div>
+          </div>
+          <div className="card_competences">
+             <div className="paragraph">
+              <h3 className="titre_un">Développeur logiciel</h3>
+              <p>DUCAT</p>
+              <p>Objet : Conceptionet développement d'une application web </p>
+              <ul><li>
+              Analyse et rédaction des spécifications techniques </li>
+                <li>Tests, recette, intégration et mise en production </li>
+                <li>
+                  Conception et réalisation de maquettes </li>
+                <li>
+                 Intégration dans l’application 
+                  </li>
+                  <li>
+                    Développement back-end application web et mobile 
+                  </li>
+                  <li>Environnement : UML, Java Spring, JS, CSS, HTML, MYSQL, IntelliJ</li>
+              </ul>
+              <hr className="date_annee"></hr>
+            <h4>Noida - Inde  - Janvier 2018 à juillet 2018</h4>
+           
+             </div>
+          </div>
+          <div className="card_competences">
+             <div className="paragraph">
+              <h3 className="titre_un">Agent de support informatique</h3>
+              <p>Beno suppor</p>
+              
+              <hr className="date_annee"></hr>
+            <h4>Noida - Inde  - Mai 2017 à décembre 2017 </h4>
+           
+             </div>
+          </div>
+          </div>
+        
+         
+          <hr className="trace"></hr>
+
+          <h4 className="parcourt">Parcourt Scolaire<GoBook/></h4>
+          <hr className="competence_hr"></hr>
+          <div className="flex">
+              <div className="card_competences">
+             <div className="paragraph">
+              <h3 className="titre_un">Mastère DevOps et Cloud</h3>
+              <p>Sup de Vinci</p>
+              <p></p>
+              <ul>
+              </ul>
+              <hr className="date_annee"></hr>
+            <h4>Courbevoie FRANCE  -Octobre 2021 à septembre 2023</h4>
+           
+             </div>
+          </div>
+          <div className="card_competences">
+             <div className="paragraph">
+              <h3 className="titre_un">Mastère 2 : Développement logiciel / mobile / IOT</h3>
+              <p>YNOV Informatique Ingesup</p>
+              <p></p>
+              <ul>
+                
+              </ul>
+              <hr className="date_annee"></hr>
+            <h4> Nanterre FRANCE- Octobre 2020 à avril 2021</h4>
+           
+             </div>
+          </div>
+          <div className="card_competences">
+             <div className="paragraph">
+              <h3 className="titre_un">Formation Expert digital architecture</h3>
+              <p>DIGITAL PARIS SCHOOL</p>
+              <p></p>
+              <ul>
+              </ul>
+              <hr className="date_annee"></hr>
+            <h4>PARIS- FRANCE-Octobre 2019 à juillet 2020</h4>
+           
+             </div>
+          </div>
+          <div className="card_competences">
+             <div className="paragraph">
+              <h3 className="titre_un">Mastère 1 : Développement logiciel / mobile / IOT</h3>
+              <p>YNOV Informatique Ingesup</p>
+              
+              <hr className="date_annee"></hr>
+            <h4> Paris - FRANCE  - Octobre 2018 à juillet 2019</h4>
+           
+             </div>
+          </div>
+          <div className="card_competences">
+             <div className="paragraph">
+              <h3 className="titre_un">Formation : Java EE </h3>
+              <p>Ducat</p>
+              
+              <hr className="date_annee"></hr>
+            <h4> NOIDA INDE  -DEC 2017 à JUIN 2018</h4>
+           
+             </div>
+          </div>
+          </div>
+        
+         
+          <div className="flex">
+              <div className="card_competences">
+             <div className="paragraph">
+              <h3 className="titre_un">Bachelor of Computer Application</h3>
+              <p>AMITY UNIVERSITY NOIDA</p>
+              <p></p>
+              <ul>
+              </ul>
+              <hr className="date_annee"></hr>
+            <h4>NOIDA INDE  -MAI 2014 à JUIN 2017</h4>
+           
+             </div>
+          </div>
+         
+        
+         
+          </div>
+        
+         
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
